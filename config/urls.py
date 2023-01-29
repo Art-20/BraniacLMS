bc2061bb599c48e8aa7915576811e2992d157d9c
Artem Sheluto <artemsheliuta@gmail.com>
2023-01-29 22:49:13 +0300

    lesson_2 Hello world with CBV and FBV

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="mainapp/")),
+    path("mainapp/", include("mainapp.urls")),
 ]
